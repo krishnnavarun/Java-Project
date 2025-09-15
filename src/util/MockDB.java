d4889ada60ba3ce3f50eb593e5117589bcf2500b
krishnnavarun <krishnavarun2024@gmail.com>
2025-09-15 10:09:09 +0530

    add MockDB.java

---
+++ src/util/MockDB.java
@@ -0,0 +1,11 @@
+package util;
+
+import model.User;
+import model.Task;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MockDB {
+    public static List<User> users = new ArrayList<>();
+    public static List<Task> tasks = new ArrayList<>();
+}

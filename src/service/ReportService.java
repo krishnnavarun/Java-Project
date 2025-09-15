55e6caa1b04c0bed67e918220a25a678b6e3400a
krishnnavarun <krishnavarun2024@gmail.com>
2025-09-15 16:02:34 +0530

    Updated reportServices.java

--- src/service/ReportService.java
+++ src/service/ReportService.java
@@ -7,5 +7,10 @@ public class ReportService {
         long pending = MockDB.tasks.stream().filter(t -> t.getStatus().equalsIgnoreCase("Pending")).count();
         long inProgress = MockDB.tasks.stream().filter(t -> t.getStatus().equalsIgnoreCase("In Progress")).count();
         long done = MockDB.tasks.stream().filter(t -> t.getStatus().equalsIgnoreCase("Done")).count();
+
+        System.out.println("=== Task Report ===");
+        System.out.println("Pending: " + pending);
+        System.out.println("In Progress: " + inProgress);
+        System.out.println("Done: " + done);
     }
 }

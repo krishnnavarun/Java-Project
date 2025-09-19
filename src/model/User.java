87954ffa57f00ff4677c9f5b316e0c5bbdadc780
krishnnavarun <krishnavarun2024@gmail.com>
2025-09-19 20:32:41 +0530

    Updated user.java

--- src/model/User.java
+++ src/model/User.java
@@ -1,9 +1,9 @@
 package model;
 
-public class User {
-    private String username;
-    private String password;
-    private String role; // Leader or Member
+public abstract class User {
+    protected String username;
+    protected String password;
+    protected String role;
 
     public User(String username, String password, String role) {
         this.username = username;
@@ -11,19 +11,9 @@ public class User {
         this.role = role;
     }
 
-    public String getUsername() {
-        return username;
-    }
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+    public String getRole() { return role; }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void showMenu() {
-        // Implemented in subclasses
-    }
+    public abstract void showMenu();
 }
